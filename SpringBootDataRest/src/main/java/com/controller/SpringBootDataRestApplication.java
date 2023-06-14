1431d7515309c710e61f77c4031ff235145ea91e
Akash Chauhan <meachauhan@gmail.com>
2023-06-14 12:59:35 +0530

    SpringDataRest Example

---
+++ SpringBootDataRest/src/main/java/com/controller/SpringBootDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataRestApplication.class, args);
+	}
+
+}
---
+++ SpringBootDataRest/src/main/java/com/controller/model/Student.java
@@ -0,0 +1,41 @@
+package com.controller.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue
+	private int sid;
+	private String sname;
+	private String sage;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSage() {
+		return sage;
+	}
+	public void setSage(String sage) {
+		this.sage = sage;
+	}
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", sage=" + sage + "]";
+	}
+	
+	
+}
